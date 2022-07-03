8bf7680c6c015c8df89f8f32390df8ea266b45c9
amine <amine.mesbahi@ostorlab.dev>
2022-07-03 20:13:10 +0200

    Set max subdomains

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,3 +53,24 @@ def test_agent2():
             args=[],
             healthcheck_port=5302)
         return dnsx_agent.DnsxAgent(definition, settings)
+
+
+@pytest.fixture
+def test_agent3():
+    with (pathlib.Path(__file__).parent.parent / 'ostorlab.yaml').open() as yaml_o:
+        definition = agent_definitions.AgentDefinition.from_yaml(yaml_o)
+        settings = runtime_definitions.AgentSettings(
+            key='agent/ostorlab/dnsx',
+            bus_url='NA',
+            bus_exchange_topic='NA',
+            redis_url='redis://redis',
+            args=[
+                utils_definitions.Arg(**{
+                    'name': 'wordlist',
+                    'type': 'string',
+                    'value': json.dumps('agent/wordlists/100_list.txt').encode()
+                }),
+                utils_definitions.Arg(name='max_subdomains', type='int', value=json.dumps(1).encode()),
+            ],
+            healthcheck_port=5301)
+        return dnsx_agent.DnsxAgent(definition, settings)
